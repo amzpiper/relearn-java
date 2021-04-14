098120cdc12dc40b7ed0ab08677a4130d388dd89
郭宇航 <1131717341@qq.com>
2021-04-14 13:05:26 +0800

    断言

---
+++ src/main/java/exception/AssertDemo.java
@@ -0,0 +1,15 @@
+package exception;
+
+public class AssertDemo {
+    public static void main(String[] args) {
+
+        //断言不能用于可恢复的程序错误，只应该用于开发和测试阶段。
+        //JVM默认关闭断言指令
+        //-enableassertions（可简写为-ea）参数启用断言
+        //实际开发中，很少使用断言。更好的方法是编写单元测试，后续我们会讲解JUnit的使用。
+        double x = Math.abs(-123.45);
+        assert x >= 0 : "x must >= 0";
+        System.out.println(x);
+
+    }
+}
--- src/main/java/exception/Exce.java
+++ src/main/java/exception/Exce.java
@@ -1,5 +1,38 @@
 package exception;
 
+import java.io.UnsupportedEncodingException;
+
 public class Exce {
 
+    public static void main(String[] args) throws Exception {
+        int code = 0;
+
+        try {
+            String s = "";
+            byte[] b= s.getBytes("GBK");
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            System.out.println("");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        System.out.println("continue");
+
+        //通过Throwable.getSuppressed()可以获取所有的Suppressed Exception。
+        //绝大多数情况下，在finally中不要抛出异常。因此，我们通常不需要关心Suppressed Exception。
+        Exception orgin = null;
+        try {
+            //Integer.parseInt("abc");
+            throw new BaseException();
+        } catch (BaseException e) {
+            orgin = e;
+            throw e;
+        } finally {
+            Exception e = new IllegalAccessException();
+            if (orgin != null) {
+                e.addSuppressed(orgin);
+            }
+            throw e;
+        }
+    }
 }
