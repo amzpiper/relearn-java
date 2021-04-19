a4be7c55f32bfdb1c9534e22a21e0e886c11ad05
peinimogu <1131717341@qq.com>
2021-04-19 23:00:09 +0800

    深入理解集合

--- src/main/java/collection/ListDemo.java
+++ src/main/java/collection/ListDemo.java
@@ -3,6 +3,7 @@ package collection;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
 public class ListDemo {
     public static void main(String[] args) {
@@ -41,6 +42,44 @@ public class ListDemo {
         //ist内部并不是通过==判断两个元素是否相等，而是使用equals()方法判断两个元素是否相等，
         //例如contains()方法可以实现如下
         System.out.println(list.contains("1"));
+        //要正确使用List的contains()、indexOf()这些方法，放入的实例必须正确覆写equals()方法，
+        //否则，放进去的实例，查找不到。我们之所以能正常放入String、Integer这些对象，
+        //是因为Java标准库定义的这些类已经正确实现了equals()方法。
+        List plist = new ArrayList<Person>();
+        plist.add(new Person("guoyuhang", 25));
+        plist.add(new Person("baobo", 24));
+        System.out.println(plist.contains(new Person("guoyuhang", 25)));
 
+        //如果不调用List的contains()、indexOf()这些方法，那么放入的元素就不需要实现equals()方法。
+    }
+}
+class Person {
+    public String name;
+    public int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Person) {
+            Person p = (Person) o;
+            //return this.name.equals(p.name) && this.age == p.age;
+            boolean nameEquals = false;
+            if (this.name == null && p.name == null) {
+                nameEquals = true;
+            }
+            if (this.name != null) {
+                nameEquals = this.name.equals(p.name);
+            }
+            //return nameEquals && this.age == age;
+
+            //Objects.equals:
+            //return (a == b) || (a != null && a.equals(b));
+            return Objects.equals(this.name, p.name) && this.age == p.age;
+        }
+        return false;
     }
 }
