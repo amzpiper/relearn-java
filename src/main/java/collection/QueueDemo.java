4c81cbce9dc0a88fded54ff635c40cc3ce67018d
郭宇航 <1131717341@qq.com>
2021-04-26 17:09:52 +0800

    PriorityQueue

---
+++ src/main/java/collection/QueueDemo.java
@@ -0,0 +1,77 @@
+package collection;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+        Queue<String> q = new LinkedList<String>();
+        // 添加3个元素到队列:
+        q.offer("apple");
+        q.offer("pear");
+        q.offer("banana");
+        // 从队列取出元素:
+        System.out.println(q.poll()); // apple
+        System.out.println(q.peek()); // pear
+        System.out.println(q.element()); // pear
+        System.out.println(q.poll()); // pear
+        System.out.println(q.poll()); // banana
+        System.out.println(q.poll()); // null,因为队列是空的
+
+        //要实现“VIP插队”的业务，用Queue就不行了，因为Queue会严格按FIFO的原则取出队首元素。我们需要的是优先队列：PriorityQueue
+        //PriorityQueue和Queue的区别在于，它的出队顺序与元素的优先级有关，对PriorityQueue调用remove()或poll()方法，返回的总是优先级最高的元素。
+        Queue<String> queue = new PriorityQueue<>();
+        // 添加3个元素到队列:
+        q.offer("apple");
+        q.offer("pear");
+        q.offer("banana");
+        System.out.println(q.poll()); // apple
+        System.out.println(q.poll()); // pear
+        System.out.println(q.poll()); // banana
+        //放入的顺序是"apple"、"pear"、"banana"
+        //取出的顺序是"apple"、"pear"、"banana"
+        //放入PriorityQueue的元素，必须实现Comparable接口，PriorityQueue会根据元素的排序顺序决定出队的优先级。
+
+        Queue<User> queue1 = new PriorityQueue<>(new UserComparator());
+        // 添加3个元素到队列:
+        queue1.offer(new User("Bob", "A1"));
+        queue1.offer(new User("Alice", "A2"));
+        queue1.offer(new User("Boss", "V1"));
+        System.out.println(queue1.poll()); // Boss/V1
+        System.out.println(queue1.poll()); // Bob/A1
+        System.out.println(queue1.poll()); // Alice/A2
+        System.out.println(queue1.poll()); // null,因为队列为空
+        //上面的UserComparator的比较逻辑其实还是有问题的，它会把A10排在A2的前面，请尝试修复该错误。
+    }
+}
+
+class UserComparator implements Comparator<User> {
+    public int compare(User u1, User u2) {
+        if (u1.number.charAt(0) == u2.number.charAt(0)) {
+            // 如果两人的号都是A开头或者都是V开头,比较号的大小:
+            return u1.number.compareTo(u2.number);
+        }
+        if (u1.number.charAt(0) == 'V') {
+            // u1的号码是V开头,优先级高:
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+}
+
+class User {
+    public final String name;
+    public final String number;
+
+    public User(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String toString() {
+        return name + "/" + number;
+    }
+}
