8636e5ca303630f40288bb1f602d43975a41e31a
郭宇航 <1131717341@qq.com>
2021-04-28 20:13:22 +0800

    File

--- src/main/java/file/FileDemo.java
+++ src/main/java/file/FileDemo.java
@@ -1,11 +1,83 @@
 package file;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //提供了File对象来操作文件和目录
-        File f = new File("C:\\Windows\\notepad.exe");
+        //可以传入绝对路径，也可以传入相对路径
+        File f = new File("C:/Windows/notepad.exe");
         System.out.println(f);
+        // 有3种形式表示的路径，一种是getPath()，返回构造方法传入的路径，
+        // 一种是getAbsolutePath()，返回绝对路径，
+        // 一种是getCanonicalPath，它和绝对路径类似，但是返回的是规范路径
+
+        //File是文件时
+        //创建&删除文件
+        try {
+            File file = new File("D:/file.txt");
+            if (file.createNewFile()) {
+                System.out.println("新增文件成功");
+                if (file.delete()) {
+                    System.out.println("删除文件成功");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //临时文件
+        File tempFile = File.createTempFile("tmp-", ".txt");
+        tempFile.deleteOnExit();//jvm退出时自动删除
+        System.out.println(tempFile.isFile());
+        System.out.println(tempFile.getAbsolutePath());
+
+        //遍历文件和目录
+        //使用list()和listFiles()列出目录下的文件和子目录名
+        //listFiles()提供了一系列重载方法，可以过滤不想要的文件和目录
+        File dir = new File("C:/Windows");
+        File[] files = dir.listFiles();//列出所有文件和子目录
+        for (File file : files) {
+            System.out.println(file.getName());
+        }
+        //过滤出所有.exe文件
+        File[] exeFiles = dir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".exe");
+            }
+        });
+
+        for (File exeFile : exeFiles) {
+            System.out.println(exeFile.getName());
+        }
+        //创建和删除目录
+        File dirFile = new File("C:/test/test1");
+        if (!(dirFile.isDirectory())) {
+            if (dirFile.mkdirs()) {
+                System.out.println("创建目录成功");
+                if (dirFile.delete()) {
+                    System.out.println("删除目录成功");
+                }
+            }
+        }
+
+        //Path
+        //Path对象，它位于java.nio.file包。Path对象和File对象类似，但操作更加简单
+        Path path1 = Paths.get(".", "project", "study");// 构造一个Path对象
+        System.out.println(path1);
+        Path path2 = path1.toAbsolutePath();// 转换为绝对路径
+        System.out.println(path2);
+        Path path3 = path2.normalize();// 转换为规范路径
+        System.out.println(path3);
+        File ff = path3.toFile();//转换为File对象
+        System.out.println(ff);
+        for (Path path : Paths.get(".").toAbsolutePath()) {
+            System.out.println(path);
+        }
     }
 }
---
+++ src/main/java/file/InputStreamDemo.java
@@ -0,0 +1,70 @@
+package file;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+//Java标准库提供的最基本的输入流
+//java.io包提供了所有同步IO的功能
+//InputStream并不是一个接口，而是一个抽象类，它是所有输入流的超类。这个抽象类定义的一个最重要的方法就是int read()
+public class InputStreamDemo {
+    public static void main(String[] args) throws IOException {
+        //new InputStream().read();
+        //方法会读取输入流的下一个字节，并返回字节表示的int值(0~255),已读到末尾，返回-1表示不能继续读取
+
+        //FileInputStream就是从文件流中读取数据
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream("./README.md");
+            for (; ; ) {
+                int n = inputStream.read();
+                if (n == -1) {
+                    break;
+                }
+                System.out.println(n);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+
+        //计算机中，类似文件、网络端口这些资源，都是由操作系统统一管理的。
+        //应用程序在运行的过程中，如果打开了一个文件进行读写，完成后要及时地关闭，
+        //以便让操作系统把资源释放掉，否则，应用程序占用的资源会越来越多，不但白白占用内存，还会影响其他应用程序的运行。
+        //关闭流就会释放对应的底层资源
+
+        //利用Java 7引入的新的try(resource)的语法
+        try (InputStream input = new FileInputStream("readme.md")) {
+            int n;
+            while ((n = input.read()) != -1) {
+                System.out.println(n);
+            }
+        }
+        //编译器在此自动为我们写入finally并调用close()
+        //编译器只看try(resource = ...)中的对象是否实现了java.lang.AutoCloseable接口，
+        //如果实现了，就自动加上finally语句并调用close()方法
+
+        //缓冲:定义一个byte[]数组作为缓冲区，read()方法会尽可能多地读取字节到缓冲区， 但不会超过缓冲区的大小
+        try(InputStream input = new FileInputStream("readme.md")) {
+            byte[] buffer = new byte[1024];
+            int n;
+            while ((n = input.read(buffer)) != -1) {//读取到缓冲区
+                System.out.println("read " + n + "bytes.");
+            }
+        }
+
+        //阻塞
+        //必须等read()方法返回后才能继续。因为读取IO流相比执行普通代码，速度会慢很多
+        byte[] data = { 72, 101, 108, 108, 111, 33 };
+        try (InputStream input = new ByteArrayInputStream(data)) {
+            int n;
+            while ((n = input.read()) != -1) {
+                System.out.println((char)n);
+            }
+        }
+    }
+}
