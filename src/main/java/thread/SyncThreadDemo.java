1c4858de2a77c2f74cb0b4ab525aed5189200d47
peinimogu <1131717341@qq.com>
2021-05-09 19:22:50 +0800

    同步方法

--- src/main/java/thread/SyncThreadDemo.java
+++ src/main/java/thread/SyncThreadDemo.java
@@ -65,6 +65,67 @@ public class SyncThreadDemo {
         System.out.println("Count:"+Counter.count);
         System.out.println("teacherCount:"+Counter.teacherCount);
         System.out.println((end-first)/1000.00);
+
+        //让线程自己选择锁对象往往会使得代码逻辑混乱，也不利于封装。更好的方法是把synchronized逻辑封装起来。
+        //例如，我们编写一个计数器如下
+        Count count = new Count();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    count.add();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    count.dec();
+                }
+            }
+        });
+        thread.start();
+        thread2.start();
+        thread.join();
+        thread2.join();
+        System.out.println(count.getN());
+        //如果一个类被设计为允许多线程正确访问，我们就说这个类就是“线程安全”的（thread-safe），
+        //上面的Count类就是线程安全的。Java标准库的java.lang.StringBuffer也是线程安全的。
+        //还有一些不变类，例如String，Integer，LocalDate，它们的所有成员变量都是final，
+        //多线程同时访问时只能读不能写，这些不变类也是线程安全的。
+        //最后，类似Math这些只提供静态方法，没有成员变量的类，也是线程安全的。
+        //除了上述几种少数情况，大部分类，例如ArrayList，都是非线程安全的类，我们不能在多线程中修改它们。
+        //但是，如果所有线程都只读取，不写入，那么ArrayList是可以安全地在线程间共享的。
+
+        //public synchronized static void test(int n) {
+        //    ...
+        //}
+        //对于static方法，是没有this实例的，因为static方法是针对类而不是实例。
+        //但是我们注意到任何一个类都有一个由JVM自动创建的Class实例，
+        //因此，对static方法添加synchronized，锁住的是该类的Class实例。
+        //上述synchronized static方法实际上相当于：
+        //public class Counter {
+        //    public static void test(int n) {
+        //        synchronized(Counter.class) {
+        //            ...
+        //        }
+        //    }
+        //}
+        //如果我们把代码稍微改一下，返回一个包含两个int的对象：
+        // public Pair get() {
+        //     Pair p = new Pair();
+        //     p.first = first;
+        //     p.last = last;
+        //     return p;
+        // }
+        // 就必须要同步了。
+
+        //小结
+        //用synchronized修饰方法可以把整个方法变为同步代码块，synchronized方法加锁对象是this；
+        //通过合理的设计和数据封装可以让一个类变为“线程安全”；
+        //一个类没有特殊说明，默认不是thread-safe；
+        //多线程能否安全访问某个非线程安全的实例，需要具体问题具体分析。
     }
 }
 
@@ -129,5 +190,25 @@ class DecTeacher extends Thread {
                 Counter.teacherCount--;
             }
         }
+    }
+}
+
+class Count {
+    private int n = 0;
+
+    public void add() {
+        synchronized (this) {
+            n++;
+        }
+    }
+
+    public void dec() {
+        synchronized (this) {
+            n--;
+        }
+    }
+
+    public int getN() {
+        return n;
     }
 }
