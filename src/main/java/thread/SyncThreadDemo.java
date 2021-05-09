daf63f19b3d43163f8e56d05dd2b5e2687a63d1e
peinimogu <1131717341@qq.com>
2021-05-09 17:28:16 +0800

    线程同步

--- src/main/java/thread/SyncThreadDemo.java
+++ src/main/java/thread/SyncThreadDemo.java
@@ -13,6 +13,9 @@ public class SyncThreadDemo {
         DecThread decThread = new DecThread();
         addThread.start();
         decThread.start();
+        addThread.join();
+        decThread.join();
+        System.out.println(Counter.count);
         //有个单线程模型下不存在的问题就来了：如果多个线程同时读写共享变量，会出现数据不一致的问题。
         //说明多线程模型下，要保证逻辑正确，对共享变量进行读写时，必须保证一组指令以原子方式执行：即某一个线程执行时，其他线程必须等待：
 
@@ -25,12 +28,51 @@ public class SyncThreadDemo {
         //    n = n + 1;
         //}
         //保证了代码块在任意时刻最多只有一个线程能执行
+        //synchronized (Counter.lock) {// 获取锁
+        //表示用Counter.lock实例作为锁，两个线程在执行各自的synchronized(Counter.lock) { ... }代码块时，
+        //必须先获得锁，才能进入代码块进行。
+        //执行结束后，在synchronized语句块结束会自动释放锁。
+        //这样一来，对Counter.count变量进行读写就不可能同时进行。上述代码无论运行多少次，最终结果都是0。
+
+        //使用synchronized解决了多线程同步访问共享变量的正确性问题。但是，它的缺点是带来了性能下降。因为synchronized代码块无法并发执行。此外，加锁和解锁需要消耗一定的时间，所以，synchronized会降低程序的执行效率。
+        //我们来概括一下如何使用synchronized：
+        //找出修改共享变量的线程代码块；
+        //选择一个共享实例作为锁；
+        //使用synchronized(lockObject) { ... }。
+        //使用synchronized的时候，不必担心抛出异常。因为无论是否有异常，都会在synchronized结束处正确释放锁
+
+        //因为两个线程各自的synchronized锁住的不是同一个对象！这使得两个线程各自都可以同时获得锁：
+        //因为JVM只保证同一个锁在任意时刻只能被一个线程获取，但两个不同的锁在同一时刻可以被两个线程分别获取。
+        //因此，使用synchronized的时候，获取到的是哪个锁非常重要。锁对象如果不对，代码逻辑就不对。
+
+        //4个线程对两个共享变量分别进行读写操作，但是使用的锁都是Counter.lock这一个对象，
+        //这就造成了原本可以并发执行的Counter.studentCount += 1和Counter.teacherCount += 1，
+        //现在无法并发执行了，执行效率大大降低。
+        //实际上，需要同步的线程可以分成两组：
+        //AddStudentThread和DecStudentThread，
+        //AddTeacherThread和DecTeacherThread，
+        //组之间不存在竞争，因此，应该使用两个不同的锁
+        System.out.println("---------------------------------------------------");
+        long first = System.currentTimeMillis();
+        Thread[] threads = new Thread[]{new AddThread(), new DecThread(), new AddTeacher(), new DecTeacher()};
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Count:"+Counter.count);
+        System.out.println("teacherCount:"+Counter.teacherCount);
+        System.out.println((end-first)/1000.00);
     }
 }
 
 class Counter {
-    static final Object lock = new Counter();
-    static int count = 0;
+    public static final Object lock = new Counter();
+    public static final Object lock2 = new Counter();
+    public static int count = 0;
+    public static int teacherCount = 0;
 }
 class AddThread extends Thread {
     @Override
@@ -57,6 +99,35 @@ class DecThread extends Thread {
                 Counter.count--;
             }// 释放锁
             System.out.println("Dec"+Counter.count);
+        }
+    }
+}
+
+class AddTeacher extends Thread {
+    @Override
+    public void run() {
+        int n = 100000;
+        for (int i = 0; i < n; i++) {
+            //使用相同锁影响并发
+            //synchronized (Counter.lock) {
+            //使用不同锁
+            synchronized (Counter.lock2) {
+                Counter.teacherCount++;
+            }
+        }
+    }
+}
+class DecTeacher extends Thread {
+    @Override
+    public void run() {
+        int n = 100000;
+        for (int i = 0; i < n; i++) {
+            //使用相同锁影响并发
+            //synchronized (Counter.lock) {
+            //使用不同锁
+            synchronized (Counter.lock2) {
+                Counter.teacherCount--;
+            }
         }
     }
 }
