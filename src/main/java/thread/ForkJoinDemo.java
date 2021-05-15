cf4af7451b4180cf319714cfa09f0c154fbbef27
peinimogu <1131717341@qq.com>
2021-05-15 21:22:58 +0800

    Fork/Join线程池

--- src/main/java/thread/ForkJoinDemo.java
+++ src/main/java/thread/ForkJoinDemo.java
@@ -1,6 +1,9 @@
 package thread;
 
 import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
 
 /**
  * @author guoyh
@@ -30,12 +33,77 @@ public class ForkJoinDemo {
         //我们来看如何使用Fork/Join对大数据进行并行求和：
         long[] array = new long[2000];
         long expectedSum = 0;
+        long startTime = System.currentTimeMillis();
         for (int i = 0; i <array.length ; i++) {
             array[i] = new Random().nextInt(1000);
             expectedSum += array[i];
         }
-        System.out.println("Expected sum: " + expectedSum);
+        long endTime = System.currentTimeMillis();
+        System.out.println(String.format("Expected sum: %d in %d ms." , expectedSum,endTime-startTime));
 
         // fork/join:
+        ForkJoinTask<Long> task = new SumTask(array, 0, array.length);
+        startTime = System.currentTimeMillis();
+        Long result = ForkJoinPool.commonPool().invoke(task);
+        endTime = System.currentTimeMillis();
+        System.out.println("Fork/join sum: " + result + " in " + (endTime - startTime) + " ms.");
+        //上述代码的执行过程，一个大的计算任务0~2000首先分裂为两个小任务0~1000和1000~2000，
+        //这两个小任务仍然太大，继续分裂为更小的0~500，500~1000，1000~1500，1500~2000，
+        //最后，计算结果被依次合并，得到最终结果。
+        //因此，核心代码SumTask继承自RecursiveTask，在compute()方法中，
+        //关键是如何“分裂”出子任务并且提交子任务：
+        //class SumTask extends RecursiveTask<Long> {
+        //    protected Long compute() {
+        //        // “分裂”子任务:
+        //        SumTask subtask1 = new SumTask(...);
+        //        SumTask subtask2 = new SumTask(...);
+        //        // invokeAll会并行运行两个子任务:
+        //        invokeAll(subtask1, subtask2);
+        //        // 获得子任务的结果:
+        //        Long subresult1 = subtask1.join();
+        //        Long subresult2 = subtask2.join();
+        //        // 汇总结果:
+        //        return subresult1 + subresult2;
+        //    }
+        //}
+        //Fork/Join线程池在Java标准库中就有应用。
+        //Java标准库提供的java.util.Arrays.parallelSort(array)可以进行并行排序，
+        //它的原理就是内部通过Fork/Join对大数组分拆进行并行排序，在多核CPU上就可以大大提高排序的速度。
+    }
+}
+
+class SumTask extends RecursiveTask<Long> {
+    static final int THRESHOLD = 500;
+    long[] array;
+    int start;
+    int end;
+
+    public SumTask(long[] array, int start, int end) {
+        this.array = array;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Long compute() {
+        // 如果任务足够小,直接计算:
+        if (end - start <= THRESHOLD) {
+            long sum = 0;
+            for (int i = 0; i < end; i++) {
+                sum += this.array[i];
+            }
+            return sum;
+        }
+        // 任务太大,一分为二:
+        int middle = (end + start) / 2;
+        System.out.println(String.format("split %d~%d ==> %d~%d,%d~%d", start, end, start, middle, middle, end));
+        SumTask sumTask1 = new SumTask(this.array, start, middle);
+        SumTask sumTask2 = new SumTask(this.array, middle, end);
+        invokeAll(sumTask1, sumTask2);
+        Long subResult1 = sumTask1.join();
+        Long subResult2 = sumTask2.join();
+        Long result = subResult1 + subResult2;
+        System.out.println("result = " + subResult1 + " + " + subResult2 + " ==> " + result);
+        return result;
     }
 }
