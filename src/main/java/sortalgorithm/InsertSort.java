de45f1a9e642ccaa593a35253ac888e412c4f4b9
郭宇航 <1131717341@qq.com>
2021-07-28 11:24:22 +0800

    fix

--- src/main/java/sortalgorithm/InsertSort.java
+++ src/main/java/sortalgorithm/InsertSort.java
@@ -1,32 +1,28 @@
 package sortalgorithm;
 
+/**
+ * 插入排序：
+ * 
+ * 最好情况时间复杂度：O(n)
+ * 最坏情况时间复杂度：O(n²) 
+ * 空间复杂度 O(1)
+ * 稳定
+ * 
+ * 工作原理是通过构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+ * 插入排序和冒泡排序一样，也有一种优化算法，叫做拆半插入。
+ * 
+ * 算法步骤：
+ * 将第一待排序序列第一个元素看做一个有序序列，把第二个元素到最后一个元素当成是未排序序列。
+ * 从头到尾依次扫描未排序序列，将扫描到的每个元素插入有序序列的适当位置。
+ * （如果待插入的元素与有序序列中的某个元素相等，则将待插入元素插入到相等元素的后面。）
+ */
 public class InsertSort {
     
     public static void main(String[] args) {
         int[] array = {78,69,55,43,31,20,10};
-        sortChange(array);
+        sortInsert(array);
     }
-    public static void sortChange(int[] array){
-        int len = array.length;
-        int minIndex,temp;
-        for (int i = 0; i < len - 1; i++) {
-            System.out.println("第"+(i+1)+"轮：");
-            
-            minIndex = i;
-            for (int j = i+1; j < len; j++) {
-                if(array[j]<array[minIndex]){
-                    minIndex = j;
-                }
-            }
-            temp = array[i];
-            array[i] = array[minIndex];
-            array[minIndex] = temp;
-
-            System.out.println(i+","+minIndex+"交换后：");
-            for (int k : array) {
-                System.out.print(k+" ");
-            }
-            System.out.println();           
-        }        
+    public static void sortInsert(int[] array){
+        
     }
 }
