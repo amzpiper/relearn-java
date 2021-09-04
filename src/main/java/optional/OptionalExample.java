cb4843aa03cb99fcf6c41e0279b0ab883841e418
amzpiper <1131717341@qq.com>
2021-09-04 17:11:16 +0800

    relearn-optional

--- src/main/java/optional/OptionalExample.java
+++ src/main/java/optional/OptionalExample.java
@@ -1,11 +1,10 @@
-package option;
+package optional;
 
-import java.security.cert.PKIXRevocationChecker.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class option {
+public class OptionalExample {
     
     /**
      * 从 Java 8 引入的一个很有趣的特性是 Optional  类。Optional 类主要解决的问题是臭名昭著的空指针异常（NullPointerException） —— 每个 Java 程序员都非常了解的异常。
